eb3029441953a0eb63f2b69d8b75cadb32918765
Solène Baudic <solene.baudic@enera.fr>
2026-05-21 14:28:21 +0200

    Fix - correct test AddOrUpdateGuestTest

--- tests/Functional/Guest/AddorUpdateGuestTest.php
+++ tests/Functional/Guest/AddorUpdateGuestTest.php
@@ -54,7 +54,7 @@ class AddOrUpdateGuestTest extends WebTestCase
     $entityManager->persist($user);
     $entityManager->flush();
 
-    $crawler = $client->request('GET', '/admin/guest/update/' . $user->getId());
+    $crawler = $client->request('GET', '/admin/guest/' . $user->getId() . 'update/');
 
     $this->assertResponseIsSuccessful();
     $this->assertSelectorExists('form');
